22ddc628b71dc759a312e5f0dae37ab7dc45aebc
leabonjean <lely40@hotmail.fr>
2016-02-29 09:15:18 +0100

    mapped

---
+++ src/main/java/echangeservices/services/MessageService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echangeservices.services;
+
+import echangeservices.entity.Message;
+import java.io.Serializable;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface MessageService extends CrudRepository<Message, Long>{
+    
+}
---
+++ src/main/java/echangeservices/services/PaiementService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echangeservices.services;
+
+import echangeservices.entity.Paiement;
+import java.io.Serializable;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface PaiementService extends CrudRepository<Paiement, Long>{
+    
+}
